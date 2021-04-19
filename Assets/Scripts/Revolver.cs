5d3b406ae63b1387db60b9cc98c5d05ba55fe4fc
Rerric <79161388+Rerric@users.noreply.github.com>
2021-04-19 19:01:29 -0400

    Added new Sprites and Scripts

---
+++ Assets/Scripts/Revolver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : MonoBehaviour
+{
+    public int[] slots;
+    public int currentSlot;
+    //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
